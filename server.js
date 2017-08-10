b7b6b192b31fd2e948877da046e2ebaf54f067d0
Joshua-Fogg <joshuafoggnz@gmail.com>
2017-08-11 10:32:51 +1200

    deconstruct server to base element

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
 
 var express = require('express')
-var app = express()
-var PORT = process.env.PORT || 3000
 
-app.listen(PORT, function () {
-  console.log('server listening on port: ', PORT)
-})
+var app = express()
 
 module.exports = app
