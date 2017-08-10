254f129eff2a17b327bf3182d25e398f857b92f7
Max Tweddell <maxt.nz@gmail.com>
2017-08-11 11:27:43 +1200

    added binary timer componenent

---
+++ client/components/Binary.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Binary (props) {
+    return <p>{decimalToBinary(props.time)} </p>
+}
+
+function decimalToBinary (number) {
+    const binaryNumber = []
+    while (x > 0) {
+        let remainder = x % 2
+        x = Math.floor(x / 2)
+        binaryNumber.push(remainder)
+    }
+    return binaryNumber.join('')
+}
