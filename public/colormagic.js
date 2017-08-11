62d2b6f4a9d47b4cba036afe7fc1d327b700bad0
Joshua-Fogg <joshuafoggnz@gmail.com>
2017-08-11 15:26:10 +1200

    add magic data js

---
+++ public/colormagic.js
@@ -0,0 +1,14 @@
+export default const magic = {
+    'box': {
+        width: '50 %',
+        height: '70 %'
+    },
+    'red': {
+        'background-color': 'red',
+        color: 'blue'
+    },
+    'blue': {
+        'background-color': 'blue',
+        color: 'red'
+    }
+}
