b4aa220078a30e088339dd36cb528e16818ca9ef
Joshua-Fogg <joshuafoggnz@gmail.com>
2017-08-11 12:01:23 +1200

    pre pull

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -5,12 +5,15 @@ class App extends Component { //Josh's Pull
   constructor (props) {
     super(props)
     this.state = {}
-  }
+    
+}
+
 
   render () {
     return (
       <div>
         Hey Josh Put Me Here.... TANKS For Comming
+        the time is: {new Date().toLocaleTimeString()}
       </div>
     )
   }
--- client/index.js
+++ client/index.js
@@ -5,7 +5,7 @@ import App from './components/App'
 
 function helloTemplate (props) {
   return (
-    <App />
+    <App/>
   )
 }
 
